7fe6fdf90edd4c36cafe6cb426d6b5fc22cb1b79
ForrestLi-666 <66054712+ForrestLi-666@users.noreply.github.com>
2020-06-02 16:27:57 +0800

    Update copy.bat

--- copy.bat
+++ copy.bat
@@ -1,1 +1,3 @@
-xcopy C:\Windows\System32\config\systemprofile\.m2\repository\demo\demo\0.0.1-SNAPSHOT\demo-0.0.1-SNAPSHOT.war D:\Jenkins\workspace\jenkins-demo_master\ /y
+echo "Start copy"
+xcopy C:\Windows\System32\config\systemprofile\.m2\repository\demo\demo\0.0.1-SNAPSHOT\demo-0.0.1-SNAPSHOT.war "D:\Jenkins\workspace\jenkins-demo_master\" /y
+echo "End copy"
