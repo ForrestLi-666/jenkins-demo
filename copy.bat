5d65429a59a40cb7340232833e7544f814294568
ForrestLi-666 <66054712+ForrestLi-666@users.noreply.github.com>
2020-06-02 16:12:49 +0800

    Update copy.bat

--- copy.bat
+++ copy.bat
@@ -1,1 +1,1 @@
-copy /y C:\\Windows\\System32\\config\\systemprofile\\.m2\\repository\\demo\\demo\\0.0.1-SNAPSHOT\\demo-0.0.1-SNAPSHOT.war D:\\Jenkins\\workspace\\jenkins-demo_master\\
+xcopy C:\Windows\System32\config\systemprofile\.m2\repository\demo\demo\0.0.1-SNAPSHOT\demo-0.0.1-SNAPSHOT.war D:\Jenkins\workspace\jenkins-demo_master\
